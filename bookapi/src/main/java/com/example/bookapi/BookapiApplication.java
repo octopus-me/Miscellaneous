2535769ff99765678527649af3811a648ead3403
Gui Wallace <guiwallace96@gmail.com>
2024-08-08 21:31:05 -0300

    Creating Spring Boot RESTful API for managing books

--- bookapi/src/main/java/com/example/bookapi/BookapiApplication.java
+++ bookapi/src/main/java/com/example/bookapi/BookapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookApiApplication {
+public class BookapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookApiApplication.class, args);
+		SpringApplication.run(BookapiApplication.class, args);
 	}
 
 }
